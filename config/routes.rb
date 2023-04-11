e611fe00a782e890fe27ef1ee284e43815edc44a
Zachary Casares <rrumble250@gmail.com>
2023-04-11 15:28:13 -0400

    add routes to models

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   resources :diaries
+  resources :foods
+  resources :users
+  resources :meals
+  resources :foods_meals
+
   root "diaries#index"
 end
