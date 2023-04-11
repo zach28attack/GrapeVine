42130685eed41db76a26b287df2923d40da45c34
Zachary Casares <rrumble250@gmail.com>
2023-04-11 19:32:22 -0400

    add form validation

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,10 @@ class Food < ApplicationRecord
 belongs_to :user
 has_and_belongs_to_many :meals
 
+validates :food_name, presence: true
+validates :calories, presence: true, numericality: { only_integer: true }
+validates :protein, allow_blank: true, numericality: { only_integer: true }
+validates :fats, allow_blank: true, numericality: { only_integer: true }
+validates :carbs, allow_blank: true, numericality: { only_integer: true }
+
 end
