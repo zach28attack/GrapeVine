af9c73b151113434b2dd9078f5a1a399c0834b72
Zachary Casares <rrumble250@gmail.com>
2023-04-11 18:43:20 -0400

    populate foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,38 @@
 class FoodsController < ApplicationController
+  def new
+    @food = Food.new
+  end
+
+  def index
+    @foods = Food.all
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = 1
+    if @food.save
+      redirect_to root_path
+    else
+      render :new
+    end
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:food_name, :calories, :protein, :fats, :carbs)
+  end
 end
