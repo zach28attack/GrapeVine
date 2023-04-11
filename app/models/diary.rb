b6e540560baa7b174efee07eeea66437384fc20f
Zachary Casares <rrumble250@gmail.com>
2023-04-11 15:48:39 -0400

    validate diary fields

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,3 +1,8 @@
 class Diary < ApplicationRecord
 belongs_to :user
+
+validates :calories, presence: true, numericality: { only_integer: true }
+validates :protein, numericality: { only_integer: true }, allow_blank: true
+validates :fats, numericality: { only_integer: true }, allow_blank: true
+validates :carbs, numericality: { only_integer: true }, allow_blank: true
 end
