1eae77d1dc1797c94e7bca1e741f1ff5c51eda16
Zachary Casares <rrumble250@gmail.com>
2023-04-11 12:35:11 -0400

    added gems: devise, factorybot, rspec

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  resources :diaries
+  root "diaries#index"
 end
