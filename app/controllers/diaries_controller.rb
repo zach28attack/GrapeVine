7b00ace473fe108066d839d6c418f97f62a0fd04
Zachary Casares <rrumble250@gmail.com>
2023-04-11 15:49:02 -0400

    populate diary controller

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,7 +1,38 @@
 class DiariesController < ApplicationController
+
   def new
     @diary = Diary.new
   end
+
   def index
+
+  end
+
+  def create
+    @diary = Diary.new(diary_params)
+    @diary.user_id = 1
+    if @diary.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:calories, :protein, :fats, :carbs, :time_of_day)
   end
 end
