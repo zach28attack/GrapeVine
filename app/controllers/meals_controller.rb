2d8837cd95e18a921719b502a4e0672175b24989
Zachary Casares <rrumble250@gmail.com>
2023-04-11 19:35:14 -0400

    populate meal controller

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,29 @@
 class MealsController < ApplicationController
+  def new
+    
+  end
+  
+  def index
+    
+  end
+
+  def show
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
 end
