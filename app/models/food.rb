2d9a7fdc79bbfbbd933899e286ab836d8dbd580e
Zachary Casares <rrumble250@gmail.com>
2023-04-12 14:06:52 -0400

    add association to diaries

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,7 @@
 class Food < ApplicationRecord
 belongs_to :user
 has_and_belongs_to_many :meals
+has_many :diaries
 
 validates :food_name, presence: true
 validates :calories, presence: true, numericality: { only_integer: true }
