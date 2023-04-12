0bc86fb07039df671c53a5e6b47b0e8d6d919bfd
Zachary Casares <rrumble250@gmail.com>
2023-04-11 20:45:02 -0400

    misc

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   resources :diaries
-  resources :foods
   resources :users
   resources :meals
+  resources :foods
   resources :foods_meals
 
   root "diaries#index"
