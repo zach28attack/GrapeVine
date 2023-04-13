c5b09c82a3baeca087db4d8019fb2724e72023c6
Zachary Casares <rrumble250@gmail.com>
2023-04-13 11:41:52 -0400

    add logic to open specific forms on certain events

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,55 +3,70 @@ import "@hotwired/turbo-rails";
 import "controllers";
 
 const diaryFormToggles = document.querySelectorAll("#diary-form-toggle");
-const diaryFormTemplate = document.querySelector(
-  "#diary-form-template"
-).content;
-const diaryFormModal = diaryFormTemplate
-  .querySelector(".diary-form-modal")
-  .cloneNode(true);
-
 const foodListFormToggles = document.querySelectorAll(
   "#add-summary-item-toggle"
 );
-const foodListFormTemplate = document.querySelector(
-  "#food-list-form-template"
-).content;
-const foodListFormModal = foodListFormTemplate
-  .querySelector(".diary-form-modal")
-  .cloneNode(true);
-
+const getTimeOfDay = (e) => {
+  return e.target.closest("#time-of-day").dataset.tod.toLowerCase();
+};
+// add event listeners to quick add buttons
 diaryFormToggles.forEach((formToggle) => {
-  formToggle.addEventListener("click", () => {
+  formToggle.addEventListener("click", (e) => {
+    // select time of day from data set in summary card to choose correct form template
+    const timeOfDay = getTimeOfDay(e);
+    const diaryFormTemplate = document.querySelector(
+      `#${timeOfDay}-diary-form-template`
+    ).content;
+    const diaryFormModal = diaryFormTemplate
+      .querySelector(".diary-form-modal")
+      .cloneNode(true);
+
+    // add form to page and add event listener to close
     document.querySelector("body").appendChild(diaryFormModal);
+    diaryFormModal.addEventListener("click", (e) => {
+      if (e.target === diaryFormModal) {
+        diaryFormModal.remove();
+      }
+    });
   });
 });
-diaryFormModal.addEventListener("click", (e) => {
-  if (e.target === diaryFormModal) {
-    diaryFormModal.remove();
-  }
-});
+
+// close modal on keypress escape
 document.addEventListener("keypress", (e) => {
   if (
     e.key === "Escape" &&
-    document.querySelector("body").contains(diaryFormModal)
+    document
+      .querySelector("body")
+      .contains(document.querySelector("#diary-form-modal"))
   ) {
-    diaryFormModal.remove();
+    document.querySelector("#diary-form-modal").remove();
   } else if (
     e.key === "Escape" &&
-    document.querySelector("body").contains(foodListFormModal)
+    document
+      .querySelector("body")
+      .contains(document.querySelector("#food-list-form-modal"))
   ) {
-    foodListFormModal.remove();
+    document.querySelector("#food-list-form-modal").remove();
   }
 });
 
+// assign event listeners to each food list form button
 foodListFormToggles.forEach((formToggle) => {
-  formToggle.addEventListener("click", () => {
-    console.log("Click");
+  formToggle.addEventListener("click", (e) => {
+    // select time of day from data set in summary card
+    const timeOfDay = getTimeOfDay(e);
+    const foodListFormTemplate = document.querySelector(
+      `#${timeOfDay}-form`
+    ).content;
+    const foodListFormModal = foodListFormTemplate
+      .querySelector(".diary-form-modal")
+      .cloneNode(true);
+    // add form modal to page and add event listener to close
     document.querySelector("body").appendChild(foodListFormModal);
+    foodListFormModal.addEventListener("click", (e) => {
+      if (e.target === foodListFormModal) {
+        foodListFormModal.remove();
+      }
+    });
   });
-});
-foodListFormModal.addEventListener("click", (e) => {
-  if (e.target === foodListFormModal) {
-    foodListFormModal.remove();
-  }
 });
