3330d6e48cd8105ed57ccb0340f484dcb3555783
Zachary Casares <rrumble250@gmail.com>
2023-04-17 18:02:37 -0400

    add foods uniqueness validation

--- app/models/foods_meal.rb
+++ app/models/foods_meal.rb
@@ -1,5 +1,7 @@
 class FoodsMeal < ApplicationRecord
-belongs_to :meal
-belongs_to :food
-belongs_to :user
+  belongs_to :meal
+  belongs_to :food
+  belongs_to :user
+  validates_uniqueness_of :food_id, scope: :meal_id
+  
 end
