7c43a10027f20281d86ce69937e6ca65051c8fd6
Zachary Casares <rrumble250@gmail.com>
2023-04-24 14:13:30 -0400

    install devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 has_many :diaries, class_name: "Diary"
 has_many :foods, class_name: "Food"
 has_many :meals, class_name: "Meal"
