f878d8ea333eb5ef334ce207faad129e322bef2e
Zachary Casares <rrumble250@gmail.com>
2023-04-25 14:26:02 -0400

    format code

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-has_many :diaries, class_name: "Diary"
-has_many :foods, class_name: "Food"
-has_many :meals, class_name: "Meal"
-has_many :foods_meals
+  has_many :diaries, class_name: "Diary"
+  has_many :foods, class_name: "Food"
+  has_many :meals, class_name: "Meal"
+  has_many :foods_meals
 
 end
