b76b3adc3ee0339138575a676da00312923d8137
Zachary Casares <rrumble250@gmail.com>
2023-04-25 14:27:03 -0400

    reformat code

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: %i[show]
   resources :diaries
-  resources :users
   resources :meals
   resources :foods
   resources :foods_meals
 
   root "diaries#index"
+  
 end
