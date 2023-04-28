2c2afbd9b2c260cccfe8d92a9a334ad55e0fa656
Zachary Casares <rrumble250@gmail.com>
2023-04-27 21:13:19 -0400

    add basic actions to controller

--- app/controllers/weight_log_controller.rb
+++ app/controllers/weight_log_controller.rb
@@ -1,2 +1,32 @@
 class WeightLogController < ApplicationController
+  
+  def new
+    
+  end
+
+  def index
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def weight_params
+    params.require(:WeightLog).permit(:log)
+  end
 end
