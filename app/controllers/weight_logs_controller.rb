23b14e5ee011b75f18bf69d82afc7db7098d9e5e
Zachary Casares <rrumble250@gmail.com>
2023-04-28 15:38:22 -0400

    add basic functions to controller

--- app/controllers/weight_logs_controller.rb
+++ app/controllers/weight_logs_controller.rb
@@ -1,14 +1,19 @@
 class WeightLogsController < ApplicationController
 
-  def new
-    
-  end
-
   def index
-    @entries = WeightLog.all
+    @weight_log = WeightLog.new
+    @logs = WeightLog.all
+    get_chart_data(@logs)
   end
 
   def create
+    weight_log = WeightLog.new(weight_log_params)
+
+    if weight_log.save
+      redirect_to weight_logs_path
+    else
+      render :index, status: :unprocessable_entity
+    end
     
   end
 
@@ -26,7 +31,19 @@ class WeightLogsController < ApplicationController
 
   private
 
-  def weight_params
-    params.require(:WeightLog).permit(:log)
+  def weight_log_params
+    params.require(:weight_log).permit(:log)
+  end
+
+  def get_chart_data(logs)
+    if logs.count > 0
+      # chart-kick requires data be in [data, data] format 
+      @chart_data = logs.map {|log| [ log.created_at, log.log ]}
+      # set the range of the charts y-axis scale
+      @min_log = logs.min_by {|log| log.log }
+      @min_chart_data = @min_log.log - 20
+      max_log = logs.max_by {|log| log.log}
+      @max_chart_data = max_log.log + 20
+    end
   end
 end
