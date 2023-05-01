6181c4106219aa4436618e8dd9f130e90bc05df6
Zachary Casares <rrumble250@gmail.com>
2023-04-30 22:53:21 -0400

    remove dead routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   root "diaries#index"
-  
+
   resources :diaries, only: %i[ index create destroy]
-  resources :foods
-  resources :foods_meals
-  resources :meals
+  resources :foods, only: %i[create destroy]
+  resources :foods_meals, only: %i[create edit destroy]
+  resources :meals, only: %i[index create edit update destroy]
   devise_for :users
   resources :users, only: %i[show]
-  resources :weight_logs
-  
-  
-  
+  resources :weight_logs, only: %i[index create destroy]
+
 end
