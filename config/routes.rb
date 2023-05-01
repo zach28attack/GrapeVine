562b8e8f55dad751e60ef0bfe71671ee4ed00b1d
Zachary Casares <rrumble250@gmail.com>
2023-05-01 16:16:08 -0400

    remove meals: edit update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :diaries, only: %i[ index create destroy]
   resources :foods, only: %i[create destroy]
   resources :foods_meals, only: %i[create edit destroy]
-  resources :meals, only: %i[index create edit update destroy]
+  resources :meals, only: %i[index create destroy]
   devise_for :users
   resources :users, only: %i[show]
   resources :weight_logs, only: %i[index create destroy]
