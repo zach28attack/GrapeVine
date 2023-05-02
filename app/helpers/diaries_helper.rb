0fccaade14ab0db543acda53d469035339865e34
Zachary Casares <rrumble250@gmail.com>
2023-05-02 14:00:06 -0400

    define a helper method

--- app/helpers/diaries_helper.rb
+++ app/helpers/diaries_helper.rb
@@ -1,2 +1,11 @@
 module DiariesHelper
+  def should_display_piechart(piechart_array)
+    truthy = false
+    piechart_array.each do |macro|
+     if macro[1] > 0
+      truthy = true 
+     end
+    end
+    truthy
+  end
 end
