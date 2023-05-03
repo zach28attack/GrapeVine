c4638988e62d45b4455b8f7a4dc9da82ee557885
Zachary Casares <rrumble250@gmail.com>
2023-05-03 14:17:33 -0400

    inculde common methods into global scope

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,24 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def remove_nil
+    if !self.protein
+      self.protein = 0
+    end
+    if !self.carbs
+      self.carbs = 0
+    end
+    if !self.fats
+      self.fats=0
+    end
+  end
+
+  def foods_macro_sum_with_servings(macro)
+    sum = 0
+    self.foods_meals.each do |fm|
+      food = Food.find(fm.food_id)
+      sum += food.send(macro) * fm.servings
+    end
+    sum
+  end
 end
