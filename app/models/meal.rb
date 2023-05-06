eaf7df9dbdbf1a8f10e7b4d7eb44c43c19c7e45e
Zachary Casares <rrumble250@gmail.com>
2023-05-06 18:56:27 -0400

    validate meal_name

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,8 +1,8 @@
 class Meal < ApplicationRecord
-belongs_to :user
-has_many :foods_meals, dependent: :destroy
-has_many :foods , through: :foods_meals, class_name: "Food"
-has_many :diaries
+  belongs_to :user
+  has_many :foods_meals, dependent: :destroy
+  has_many :foods , through: :foods_meals, class_name: "Food"
+  has_many :diaries
 
-  
+  validates :meal_name, presence: true
 end
