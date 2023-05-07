50349146d0216e0fd722ea3d11342abf9cd9f768
Zachary Casares <rrumble250@gmail.com>
2023-05-07 13:36:03 -0400

    remove greater than 1 validation for calories

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -3,7 +3,7 @@ belongs_to :user
 belongs_to :meal, optional:true
 belongs_to :food, optional:true
 
-validates :calories, presence: true, numericality: { only_integer: true, greater_than: 0 }
+validates :calories, presence: true, numericality: { only_integer: true}
 validates :protein, numericality: { only_integer: true }, allow_blank: true
 validates :fats, numericality: { only_integer: true }, allow_blank: true
 validates :carbs, numericality: { only_integer: true }, allow_blank: true
