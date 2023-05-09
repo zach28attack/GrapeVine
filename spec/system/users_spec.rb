f800080fb5bd9e587c7e522b6b7121f858dba716
Zachary Casares <rrumble250@gmail.com>
2023-05-08 21:45:48 -0400

    configure system tests

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
   before do
-    driven_by(:rack_test)
+    driven_by :selenium, using: :safari
   end
 
   pending "add some scenarios (or delete) #{__FILE__}"
