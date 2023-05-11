aee0a8bb350e9d32d0849c68a0da4c5631f0c393
Zachary Casares <rrumble250@gmail.com>
2023-05-11 12:52:11 -0400

    remove debug code

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -9,15 +9,11 @@ RSpec.describe "Users", type: :system do
     context "with valid attributes" do
       it "should create new user" do 
         expect{
-          print User.count
           visit new_user_registration_path
           fill_in "Email", with: "newEmail@gmail.com"
           fill_in "Password", with: "password123"
           fill_in "Password confirmation", with: "password123"
-          button = find('input[name="commit"]')
-          button.click
-          print User.count
-
+          find('input[name="commit"]').click
         }.to change(User, :count).by(1)
       end
     end
