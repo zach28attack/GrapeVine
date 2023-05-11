2243c7f8be0e8d06bd7f11cf80b548cbfbe98301
Zachary Casares <rrumble250@gmail.com>
2023-05-11 12:51:30 -0400

    change browser to run system spec on

--- spec/system/foods_meals_spec.rb
+++ spec/system/foods_meals_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "FoodsMeals", type: :system do
   before do
-    driven_by :selenium, using: :safari
+    driven_by :selenium_chrome, using: :chrome
   end
 
   pending "add some scenarios (or delete) #{__FILE__}"
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,8 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
   before do
-    driven_by :selenium, using: :safari
+    driven_by :selenium_chrome, using: :chrome
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe "sign-up" do
+    context "with valid attributes" do
+      it "should create new user" do 
+        expect{
+          print User.count
+          visit new_user_registration_path
+          fill_in "Email", with: "newEmail@gmail.com"
+          fill_in "Password", with: "password123"
+          fill_in "Password confirmation", with: "password123"
+          button = find('input[name="commit"]')
+          button.click
+          print User.count
+
+        }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
